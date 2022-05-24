169bef17a43f6bb1ef06f86af9582ea3a50996ea
joão victor andrade <jvasantos2005@gmail.com>
2022-05-24 09:40:58 -0300

    APANHAMOS PARA FUNÇÃO DE MERDA

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,21 +1,23 @@
 import { Router } from 'express'
 
-import { dobro, somar } from './services.js';
+import { dobro, somar, media, ingresso, corprimaria } from './services.js';
 
 const server = Router();
 
-server.get('/ping', (req, resp) => {
-    resp.send('pong'); 
-})
-
 server.get('/dobro/:numero', (req, resp)=>{
+    try{    
         let numero = Number(req.params.numero);
         
         const d = dobro(numero);
           
           resp.send({
                 dobro: d    
-});
+        });
+    }catch(err){
+        resp.send({
+            erro: err.message
+        })
+    }
 
 })
 
@@ -56,5 +58,76 @@ server.post('/somar', (req, resp) => {
         })
     }
 })
+
+server.get('/media', (req, resp) => {
+    try{
+        const a = Number(req.query.a);
+        const b = Number(req.query.b);
+        const c = Number(req.query.c);
+
+        const m = media(a, b, c); 
+
+            resp.send({
+                med: m
+            })
+    }catch(err){
+        resp.send({
+            erro: err.message
+        })
+
+    }
+    
+})
+
+server.post('/tabu', (req, resp) =>{
+    try{
+        let { n1 } = req.body;
+        let r = tabu(n1);
+
+        resp.send({
+            tabuada: r
+        })
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.post('/dia2/IngressoCinema', (req , resp) => {
+    try{
+        const {a, b, c, d} = req.body;
+
+        let x = ingresso(a, b, c, d);
+        
+        resp.send({
+                ingr: x
+        })
+    }catch(err){
+        resp.send({
+        erro: err.message
+    })
+}
+})
+
+server.get('/dia2/corprimaria/:cor',(req, resp) => {
+    try {
+        const d = req.params.cor;
+
+        let b = corprimaria(d);
+        
+        resp.send({
+            primaria: b
+        })
+
+    }catch(err){
+            resp.send({
+                erro: err.message
+            })
+    }
+})
+
+
+
 
 export default server;
--- src/services.js
+++ src/services.js
@@ -5,3 +5,33 @@ export function somar(a, b ){
 export function dobro (n){
     return n * 2;
 }
+
+export function media (n1, n2, n3){
+    return (n1 + n2 + n3) / 3;
+}
+
+
+export function ingresso(inteiro, meia, dia, nacio){
+    let m = 14.25;
+    let i = 28.50;
+    if(nacio == "brasileiro"){
+        m = 5;
+        i = 5;
+    }
+    else if(dia == "quarta"){
+        i = i / 2;
+    }
+    let s = inteiro * i;
+    let y = m * meia;
+    let total = s + y;
+    
+    return total;
+}
+
+export function corprimaria(cor){
+    
+    let primaria = cor == 'azul' || cor == 'vermelho' || cor == 'amarelo';
+
+    return primaria;
+}
+
